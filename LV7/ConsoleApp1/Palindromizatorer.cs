2f39ec3d5bad9a3696d2d12ae739d78e12fabd34
PhatPhuckDave <kosmodiskclassic0@gmail.com>
2021-05-19 14:50:21 +0200

    Completed LV7

---
+++ LV7/ConsoleApp1/Palindromizatorer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindromizator {
+    public class Palindromizatorer {
+        private string GetReverseString(string input) {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private void RemoveWhitespace(ref string input) {
+            input = String.Concat(input.Where(c => !Char.IsWhiteSpace(c)));
+        }
+
+        private void RemoveSpecialCharacters(ref string input) {
+            input = String.Concat(input.Where(c => !Char.IsPunctuation(c)));
+            input = String.Concat(input.Where(c => !Char.IsSymbol(c)));
+        }
+
+        private void RemoveDigits(ref string input) {
+            input = String.Concat(input.Where(c => !Char.IsDigit(c)));
+        }
+
+        public bool IsPalindrom(string message) {
+            if (message.Length < 1)
+                throw new ArgumentException($"Invalid entry ({message} - string might be empty)");
+
+            message = message.ToLower();
+            RemoveWhitespace(ref message);
+            RemoveSpecialCharacters(ref message);
+            // Brojevi?
+            // RemoveDigits(ref message);
+            var messageReverse = GetReverseString(message);
+
+            return String.Equals(message, messageReverse);
+        }
+
+        public static void Main() {
+        }
+    }
+}
---
+++ LV7/Palindromizator.test/PalindromizatorTester.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Palindromizator.Test {
+    [TestFixture]
+    class PalindromizatorTester {
+        [TestCase("Anna")]
+        [TestCase("oko")]
+        [TestCase("Kisik")]
+        [TestCase("neven")]
+        [TestCase("ImaJu ArAPi I paRa u jAMi")]
+        [TestCase("1331")]
+        public void IsPalindrom_WhenInputIsPalindrom_ReturnsTrue(string message) {
+            var palindromizatorer = new Palindromizatorer();
+            bool expected = true;
+
+            bool actual = palindromizatorer.IsPalindrom(message);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        // Ime?
+        public void IsPalindrom_WhenInputHasSpecialCharacter_ReturnsIsPalindrom() {
+            var palindromizatorer = new Palindromizatorer();
+            var message = "Nakrao Arkan!";
+            bool expected = true;
+
+            bool actual = palindromizatorer.IsPalindrom(message);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsPalindrom_WhenInputIsLoremIpsum_ReturnsFalse() {
+            var palindromizatorer = new Palindromizatorer();
+            string message = "LoremIpsum";
+            bool expected = false;
+
+            bool actual = palindromizatorer.IsPalindrom(message);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsPalindrom_WhenInputIsAna_Voli_Milovana_ReturnsTrue() {
+            var palindromizatorer = new Palindromizatorer();
+            string message = "Ana voli Milovana";
+            bool expected = true;
+
+            bool actual = palindromizatorer.IsPalindrom(message);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsPalindrom_WhenInputIsEmpty_ThrowsArgumentException() {
+            var palindromizatorer = new Palindromizatorer();
+            string message = "";
+            Assert.Throws<ArgumentException>(() => palindromizatorer.IsPalindrom(message));
+        }
+    }
+}
